4124a10e300da706bc01ef36ae18ac81002a8ab6
Mary Joe <maryjoekhoury@outlook.com>
2024-07-09 10:00:47 +0300

    change in hero style

--- components/BannerMessage.js
+++ components/BannerMessage.js
@@ -3,13 +3,13 @@ import AlignContainer from "./AlignContainer";
 
 const BannerMessage = () => {
   return (
-    <div className=" text-center md:text-left  ">
+    <div className=" text-center md:text-left ">
       <h1 className="text-4xl font-bold sm:text-5xl  ">
         {/* {props.slogan} */}
         Search, Find , & Apply
       </h1>
       {/* <p>{props.description}</p> */}
-      <p className="text-lg  sm:text-xl md:mt-8 md:mb-14 mt-6 mb-9 md:w-1/2  ">
+      <p className="text-lg  sm:text-xl md:mt-8 md:mb-14 mt-6 mb-9  md:w-9/12 ">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam.
--- components/HeroArea.js
+++ components/HeroArea.js
@@ -7,28 +7,41 @@ import BannerMessage from "./BannerMessage";
 const HeroArea = (props) => {
   return (
     <>
-      <div className="">
+      <div className="bg-red-500">
         {/* <div className="lg:bg-[url('/images/hero.png')] bg-no-repeat bg-center ">/ */}
         <NavBar />
-        <img
-          src="/images/hero.png"
-          className="w-full md:block hidden absolute top-0 right-0 z-[-1]"
-        />
-        <AlignContainer>
-          <div className="flex flex-col justify-center md:items-start items-center pt-16 md:pt-44 bg-[#FBFBFB] md:bg-opacity-0">
-            <BannerMessage />
-            <SearchJobFilter />
+        <div className="mb-4 flex-wrap md:bg-grey bg-[#FBFBFB]">
+          <AlignContainer>
+            <div className="flex flex-col md:items-center  md:flex-row xl:py-[202px] lg:py-[190px] md:py-48">
+              <div className="md:w-9/12 ">
+                <BannerMessage />
+                <SearchJobFilter />
+              </div>
+            </div>
+          </AlignContainer>
+          <div className=" w-full">
+            <img
+              src="/images/hero-image.png"
+              className=" hidden md:block absolute top-0 xl:right-[-50px] lg:right-[-270px] md:right-[-550px]"
+            />
+            <img
+              src="/images/hero-image-mobile.png"
+              className=" block md:hidden w-full mt-4"
+            />
           </div>
-        </AlignContainer>
-
-        <div className=" w-full">
-          <img
-            src="/images/hero-image-mobile.png"
-            className=" block md:hidden w-full "
-          />
         </div>
+
+        {/* <div className=" w-full">
+            <img
+              src="/images/hero-image.png"
+              className=" md:block hidden absolute top-0 right-0 z-[-1]"
+            />
+            <img
+              src="/images/hero-image-mobile.png"
+              className=" block md:hidden w-full "
+            />
+          </div> */}
       </div>
-      {/* </div> */}
     </>
   );
 };
